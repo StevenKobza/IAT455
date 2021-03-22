0772085ec5ad9dffd8ad8f7168f5a0dcdb3f2a95
Victoria <viki.maslova@gmail.com>
2021-03-22 14:51:59 -0700

    UI iteration 1
    Changed window size to 1080*720, updated an aligned placement of buttons and file selection labels.

--- march22Demo/tkinterStitching.py
+++ march22Demo/tkinterStitching.py
@@ -15,29 +15,29 @@ modes = (cv.Stitcher_PANORAMA, cv.Stitcher_SCANS)
 class Application(Frame):
     def __init__ (self):
         self.main_window = Tk()
-        self.main_window.geometry("900x900")
-        self.main_window.title("March 22nd Demo")
+        self.main_window.geometry("1080x720")
+        self.main_window.title("Panorama Stitcher")
 
-        self.mainFrame = Frame(self.main_window, width = 900, height = 900)
+        self.mainFrame = Frame(self.main_window, width = 1080, height = 720)
         self.mainFrame.place(x = 0, y = 0)
         
-        self.chooseImagesButton = Button(self.mainFrame, text = "Choose at least two images please", font = ("Arial", 16), command = self.chooseImages)
-        self.chooseImagesButton.place(x = 100, y = 200)
+        self.chooseImagesButton = Button(self.mainFrame, text = "Select at least 2 images", font = ("Arial", 14), command = self.chooseImages)
+        self.chooseImagesButton.place(x = 50, y = 100)
 
         self.imagesChosen = StringVar()
         self.imageSaved = StringVar()
 
-        self.imagesChosenLabel = Label(self.mainFrame, textvariable = self.imagesChosen, font = ("Arial", 16))
-        self.imagesChosenLabel.place(x = 150, y = 150)
+        self.imagesChosenLabel = Label(self.mainFrame, textvariable = self.imagesChosen, font = ("Arial", 12))
+        self.imagesChosenLabel.place(x = 50, y = 150)
         
-        self.saveImageButton = Button(self.mainFrame, text = "Please select a save location", font = ("Arial", 16), command = self.saveImage)
-        self.saveImageButton.place(x = 400, y = 400)
+        self.saveImageButton = Button(self.mainFrame, text = "Choose your save location", font = ("Arial", 14), command = self.saveImage)
+        self.saveImageButton.place(x = 50, y = 220)
 
-        self.saveImageLabel = Label(self.mainFrame, textvariable = self.imageSaved, font = ("Arial", 16))
-        self.saveImageLabel.place(x = 500, y = 300)
+        self.saveImageLabel = Label(self.mainFrame, textvariable = self.imageSaved, font = ("Arial", 12))
+        self.saveImageLabel.place(x = 50, y = 270)
 
-        self.panoramaButton = Button(self.mainFrame, text = "Press to make panorama", font = ("Arial", 16), command = self.runPanorama)
-        self.panoramaButton.place(x = 400, y = 600)
+        self.panoramaButton = Button(self.mainFrame, text = "Compile Panorama", font = ("Arial", 14), command = self.runPanorama)
+        self.panoramaButton.place(x = 50, y = 400)
 
         mainloop()
 
